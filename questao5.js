7cfaa366d3df10bc748e7d8c2b46f6ea0e25a845
thaua-arruda <thauafarruda@gmail.com>
2024-04-03 16:51:30 -0400

    feita questao 5

---
+++ questao5.js
@@ -0,0 +1,13 @@
+/*
+Questão 5: Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda
+coluna em JavaScript?
+*/
+
+let matriz = [
+    [10,20,30],
+    [40,50,60],
+    [70,80,90],
+];
+
+console.log(matriz[2][1]);
+
