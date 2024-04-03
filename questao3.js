e6a4d3a6d8d864a6def6f50d70e068bb65f57560
thaua-arruda <thauafarruda@gmail.com>
2024-04-03 16:51:21 -0400

    feita questao 3

---
+++ questao3.js
@@ -0,0 +1,9 @@
+/*
+Questão 3: Crie um vetor com 10 elementos e remova o último elemento e imprima
+o vetor sem o elemento removido em JavaScript?
+*/
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+array.pop();
+console.log(array);
